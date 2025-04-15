e69fa8370c062ec823241f91ea47a9f5addda160
Dennis Mwape <bashinamwila@gmail.com>
2025-04-15 16:33:03 +0200

    Add comments to clarify IItemAdditionStep behavior
    Enhance the `Execute` method with comments explaining that
    steps implementing the `IItemAdditionStep` interface are
    not considered complete unless all item properties are set.
    Additionally, clarify that in cases where the item addition
    multi-step is complete, the item may not have its properties
    set, resulting in the step being marked as incomplete.

--- src/CustomerOnboarding.BusinessLibrary/Rules/CheckIfMultiStepIsComplete.cs
+++ src/CustomerOnboarding.BusinessLibrary/Rules/CheckIfMultiStepIsComplete.cs
@@ -24,6 +24,11 @@ namespace CustomerOnboarding.BusinessLibrary.Rules
             var target = context.Target as IOnboardingOrchestrator;
             if (target is not null)
             {
+                //steps which implement the IItemAdditionStep interface
+                //are not considered complete unless the item has all it's 
+                //properties set.But in the event the item addition multi step
+                // is complete the item will not have it's properties set
+                //thereby rendering the step incomplete.
                 var isComplete = target.Steps
                     .Where(step => step is not IItemAdditionStep)
                     .All(step => step.IsCompleted);
